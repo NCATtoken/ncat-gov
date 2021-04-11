c906193efda9476986f3ca33ee2227a2584547a0
yinsee <yinsee@3b.my>
2021-04-11 15:18:17 +0800

    improve display show shorten address

---
+++ client/src/components/address/index.js
@@ -0,0 +1,15 @@
+import Typography from "@material-ui/core/Typography";
+import { Fragment, useEffect, useState } from "react";
+
+// TODO: Web Design
+function Address({ address }) {
+  var display = address === null ? '' : (address.substr(0, 6) + "..." + address.substr(-4, 4));
+  return (
+    <Fragment>
+      <span title={address}>
+        {display}
+      </span>
+    </Fragment>
+  );
+}
+export default Address;
